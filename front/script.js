8d31b7b14d07b3a497830e7891fbbe22ac64448b
Erfan Abedi <erfanabedi.78@gmail.com>
2023-11-28 12:47:52 -0800

    savepoint

--- front/script.js
+++ front/script.js
@@ -1,3 +1,5 @@
+let pdfDoc = null;
+
 document.getElementById('file-input').addEventListener('change', (event) => {
     const file = event.target.files[0];
     if (file.type !== 'application/pdf') {
@@ -7,12 +9,15 @@ document.getElementById('file-input').addEventListener('change', (event) => {
 
     const fileReader = new FileReader();
 
-    fileReader.onload = function() {
+    fileReader.onload = function () {
         const typedArray = new Uint8Array(this.result);
 
         // Load the PDF file
-        pdfjsLib.getDocument({data: typedArray}).promise.then(pdf => {
+        pdfjsLib.getDocument({ data: typedArray }).promise.then(pdf => {
             console.log('PDF loaded');
+
+            // Save the PDF file into a global object
+            pdfDoc = pdf;
 
             // iterate through all pages
             for (let i = 1; i <= pdf.numPages; i++) {
@@ -21,7 +26,7 @@ document.getElementById('file-input').addEventListener('change', (event) => {
                     console.log('Page loaded');
 
                     const scale = 1.5;
-                    const viewport = page.getViewport({scale: scale});
+                    const viewport = page.getViewport({ scale: scale });
 
                     // Prepare canvas using PDF page dimensions
                     const canvas = document.createElement('canvas');
@@ -32,7 +37,7 @@ document.getElementById('file-input').addEventListener('change', (event) => {
                     canvas.style.marginLeft = 'auto';
                     canvas.style.marginRight = 'auto';
                     canvas.style.display = 'block';
-                    
+
 
                     // Render PDF page into canvas context
                     const renderContext = {
@@ -47,7 +52,7 @@ document.getElementById('file-input').addEventListener('change', (event) => {
                 });
             }
 
-            
+
         }, function (reason) {
             // PDF loading error
             console.error(reason);
@@ -56,3 +61,31 @@ document.getElementById('file-input').addEventListener('change', (event) => {
 
     fileReader.readAsArrayBuffer(file);
 });
+
+document.getElementById('delete-page').addEventListener('click', async () => {
+    const pageNumber = parseInt(document.getElementById('page-number').value);
+    if (pageNumber > 0 && pageNumber <= pdfDoc.getPageCount()) {
+        pdfDoc.removePage(pageNumber - 1);
+        const pdfBytes = await pdfDoc.save();
+
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+
+        const downloadLink = document.getElementById('download-link');
+        downloadLink.href = url;
+        downloadLink.download = 'modified_document.pdf';
+        downloadLink.style.display = 'block';
+        downloadLink.textContent = 'Download Modified PDF';
+    } else {
+        alert('Invalid page number');
+    }
+});
+
+// const file = event.target.files[0];
+//     if (file.type !== 'application/pdf') {
+//         console.error(file.name, 'is not a pdf file.');
+//         return;
+//     }
+
+//     const arrayBuffer = await file.arrayBuffer();
+//     const pdfDoc = await PDFLib.PDFDocument.load(arrayBuffer);
