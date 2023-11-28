468199a5429c925180b7e6511771a1fc23e78808
Erfan Abedi <erfanabedi.78@gmail.com>
2023-11-28 12:05:34 -0800

    center the pdf

--- front/script.js
+++ front/script.js
@@ -13,31 +13,41 @@ document.getElementById('file-input').addEventListener('change', (event) => {
         // Load the PDF file
         pdfjsLib.getDocument({data: typedArray}).promise.then(pdf => {
             console.log('PDF loaded');
-            
-            // Fetch the first page
-            pdf.getPage(1).then(page => {
-                console.log('Page loaded');
-                
-                const scale = 1.5;
-                const viewport = page.getViewport({scale: scale});
 
-                // Prepare canvas using PDF page dimensions
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
+            // iterate through all pages
+            for (let i = 1; i <= pdf.numPages; i++) {
+                // Fetch the page
+                pdf.getPage(i).then(page => {
+                    console.log('Page loaded');
 
-                // Render PDF page into canvas context
-                const renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-                const renderTask = page.render(renderContext);
-                renderTask.promise.then(() => {
-                    console.log('Page rendered');
-                    document.getElementById('pdf-container').appendChild(canvas);
+                    const scale = 1.5;
+                    const viewport = page.getViewport({scale: scale});
+
+                    // Prepare canvas using PDF page dimensions
+                    const canvas = document.createElement('canvas');
+                    const context = canvas.getContext('2d');
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+                    // Center canvas
+                    canvas.style.marginLeft = 'auto';
+                    canvas.style.marginRight = 'auto';
+                    canvas.style.display = 'block';
+                    
+
+                    // Render PDF page into canvas context
+                    const renderContext = {
+                        canvasContext: context,
+                        viewport: viewport
+                    };
+                    const renderTask = page.render(renderContext);
+                    renderTask.promise.then(() => {
+                        console.log('Page rendered');
+                        document.getElementById('pdf-container').appendChild(canvas);
+                    });
                 });
-            });
+            }
+
+            
         }, function (reason) {
             // PDF loading error
             console.error(reason);
