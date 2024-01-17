3a0053c843f833dc12ae92ec920eb142df1c565c
Erfan Abedi <erfanabedi.78@gmail.com>
2024-01-16 17:34:15 -0800

    making some progress towards non-webpacking, just a little more

--- webpack/js/web/pdf_thumbnail_viewer.js
+++ webpack/js/web/pdf_thumbnail_viewer.js
@@ -1,65 +1,121 @@
+/* Copyright 2012 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-;// CONCATENATED MODULE: ./web/pdf_thumbnail_viewer.js
+/** @typedef {import("../src/display/api").PDFDocumentProxy} PDFDocumentProxy */
+/** @typedef {import("../src/display/api").PDFPageProxy} PDFPageProxy */
+/** @typedef {import("./event_utils").EventBus} EventBus */
+/** @typedef {import("./interfaces").IPDFLinkService} IPDFLinkService */
+// eslint-disable-next-line max-len
+/** @typedef {import("./pdf_rendering_queue").PDFRenderingQueue} PDFRenderingQueue */
 
+import {
+  getVisibleElements,
+  isValidRotation,
+  RenderingStates,
+  scrollIntoView,
+  watchScroll,
+} from "./ui_utils.js";
+import { PDFThumbnailView, TempImageFactory } from "./pdf_thumbnail_view.js";
 
 const THUMBNAIL_SCROLL_MARGIN = -19;
 const THUMBNAIL_SELECTED_CLASS = "selected";
+
+/**
+ * @typedef {Object} PDFThumbnailViewerOptions
+ * @property {HTMLDivElement} container - The container for the thumbnail
+ *   elements.
+ * @property {EventBus} eventBus - The application event bus.
+ * @property {IPDFLinkService} linkService - The navigation/linking service.
+ * @property {PDFRenderingQueue} renderingQueue - The rendering queue object.
+ * @property {Object} [pageColors] - Overwrites background and foreground colors
+ *   with user defined ones in order to improve readability in high contrast
+ *   mode.
+ */
+
+/**
+ * Viewer control to display thumbnails for pages in a PDF document.
+ */
 class PDFThumbnailViewer {
+  /**
+   * @param {PDFThumbnailViewerOptions} options
+   */
   constructor({
     container,
     eventBus,
     linkService,
     renderingQueue,
-    pageColors
+    pageColors,
   }) {
     this.container = container;
     this.eventBus = eventBus;
     this.linkService = linkService;
     this.renderingQueue = renderingQueue;
     this.pageColors = pageColors || null;
+
     this.scroll = watchScroll(this.container, this._scrollUpdated.bind(this));
     this._resetView();
   }
+
+  /**
+   * @private
+   */
   _scrollUpdated() {
     this.renderingQueue.renderHighestPriority();
   }
+
   getThumbnail(index) {
     return this._thumbnails[index];
   }
+
+  /**
+   * @private
+   */
   _getVisibleThumbs() {
     return getVisibleElements({
       scrollEl: this.container,
-      views: this._thumbnails
+      views: this._thumbnails,
     });
   }
+
   scrollThumbnailIntoView(pageNumber) {
     if (!this.pdfDocument) {
       return;
     }
     const thumbnailView = this._thumbnails[pageNumber - 1];
+
     if (!thumbnailView) {
       console.error('scrollThumbnailIntoView: Invalid "pageNumber" parameter.');
       return;
     }
+
     if (pageNumber !== this._currentPageNumber) {
       const prevThumbnailView = this._thumbnails[this._currentPageNumber - 1];
+      // Remove the highlight from the previous thumbnail...
       prevThumbnailView.div.classList.remove(THUMBNAIL_SELECTED_CLASS);
+      // ... and add the highlight to the new thumbnail.
       thumbnailView.div.classList.add(THUMBNAIL_SELECTED_CLASS);
     }
-    const {
-      first,
-      last,
-      views
-    } = this._getVisibleThumbs();
+    const { first, last, views } = this._getVisibleThumbs();
+
+    // If the thumbnail isn't currently visible, scroll it into view.
     if (views.length > 0) {
       let shouldScroll = false;
       if (pageNumber <= first.id || pageNumber >= last.id) {
         shouldScroll = true;
       } else {
-        for (const {
-          id,
-          percent
-        } of views) {
+        for (const { id, percent } of views) {
           if (id !== pageNumber) {
             continue;
           }
@@ -68,16 +124,17 @@ class PDFThumbnailViewer {
         }
       }
       if (shouldScroll) {
-        scrollIntoView(thumbnailView.div, {
-          top: THUMBNAIL_SCROLL_MARGIN
-        });
+        scrollIntoView(thumbnailView.div, { top: THUMBNAIL_SCROLL_MARGIN });
       }
     }
+
     this._currentPageNumber = pageNumber;
   }
+
   get pagesRotation() {
     return this._pagesRotation;
   }
+
   set pagesRotation(rotation) {
     if (!isValidRotation(rotation)) {
       throw new Error("Invalid thumbnails rotation angle.");
@@ -86,16 +143,16 @@ class PDFThumbnailViewer {
       return;
     }
     if (this._pagesRotation === rotation) {
-      return;
+      return; // The rotation didn't change.
     }
     this._pagesRotation = rotation;
-    const updateArgs = {
-      rotation
-    };
+
+    const updateArgs = { rotation };
     for (const thumbnail of this._thumbnails) {
       thumbnail.update(updateArgs);
     }
   }
+
   cleanup() {
     for (const thumbnail of this._thumbnails) {
       if (thumbnail.renderingState !== RenderingStates.FINISHED) {
@@ -104,70 +161,104 @@ class PDFThumbnailViewer {
     }
     TempImageFactory.destroyCanvas();
   }
+
+  /**
+   * @private
+   */
   _resetView() {
     this._thumbnails = [];
     this._currentPageNumber = 1;
     this._pageLabels = null;
     this._pagesRotation = 0;
+
+    // Remove the thumbnails from the DOM.
     this.container.textContent = "";
   }
+
+  /**
+   * @param {PDFDocumentProxy} pdfDocument
+   */
   setDocument(pdfDocument) {
     if (this.pdfDocument) {
       this._cancelRendering();
       this._resetView();
     }
+
     this.pdfDocument = pdfDocument;
     if (!pdfDocument) {
       return;
     }
     const firstPagePromise = pdfDocument.getPage(1);
     const optionalContentConfigPromise = pdfDocument.getOptionalContentConfig();
-    firstPagePromise.then(firstPdfPage => {
-      const pagesCount = pdfDocument.numPages;
-      const viewport = firstPdfPage.getViewport({
-        scale: 1
+
+    firstPagePromise
+      .then(firstPdfPage => {
+        const pagesCount = pdfDocument.numPages;
+        const viewport = firstPdfPage.getViewport({ scale: 1 });
+
+        for (let pageNum = 1; pageNum <= pagesCount; ++pageNum) {
+          const thumbnail = new PDFThumbnailView({
+            container: this.container,
+            eventBus: this.eventBus,
+            id: pageNum,
+            defaultViewport: viewport.clone(),
+            optionalContentConfigPromise,
+            linkService: this.linkService,
+            renderingQueue: this.renderingQueue,
+            pageColors: this.pageColors,
+          });
+          this._thumbnails.push(thumbnail);
+        }
+        // Set the first `pdfPage` immediately, since it's already loaded,
+        // rather than having to repeat the `PDFDocumentProxy.getPage` call in
+        // the `this.#ensurePdfPageLoaded` method before rendering can start.
+        this._thumbnails[0]?.setPdfPage(firstPdfPage);
+
+        // Ensure that the current thumbnail is always highlighted on load.
+        const thumbnailView = this._thumbnails[this._currentPageNumber - 1];
+        thumbnailView.div.classList.add(THUMBNAIL_SELECTED_CLASS);
+      })
+      .catch(reason => {
+        console.error("Unable to initialize thumbnail viewer", reason);
       });
-      for (let pageNum = 1; pageNum <= pagesCount; ++pageNum) {
-        const thumbnail = new PDFThumbnailView({
-          container: this.container,
-          eventBus: this.eventBus,
-          id: pageNum,
-          defaultViewport: viewport.clone(),
-          optionalContentConfigPromise,
-          linkService: this.linkService,
-          renderingQueue: this.renderingQueue,
-          pageColors: this.pageColors
-        });
-        this._thumbnails.push(thumbnail);
-      }
-      this._thumbnails[0]?.setPdfPage(firstPdfPage);
-      const thumbnailView = this._thumbnails[this._currentPageNumber - 1];
-      thumbnailView.div.classList.add(THUMBNAIL_SELECTED_CLASS);
-    }).catch(reason => {
-      console.error("Unable to initialize thumbnail viewer", reason);
-    });
   }
+
+  /**
+   * @private
+   */
   _cancelRendering() {
     for (const thumbnail of this._thumbnails) {
       thumbnail.cancelRendering();
     }
   }
+
+  /**
+   * @param {Array|null} labels
+   */
   setPageLabels(labels) {
     if (!this.pdfDocument) {
       return;
     }
     if (!labels) {
       this._pageLabels = null;
-    } else if (!(Array.isArray(labels) && this.pdfDocument.numPages === labels.length)) {
+    } else if (
+      !(Array.isArray(labels) && this.pdfDocument.numPages === labels.length)
+    ) {
       this._pageLabels = null;
       console.error("PDFThumbnailViewer_setPageLabels: Invalid page labels.");
     } else {
       this._pageLabels = labels;
     }
+    // Update all the `PDFThumbnailView` instances.
     for (let i = 0, ii = this._thumbnails.length; i < ii; i++) {
       this._thumbnails[i].setPageLabel(this._pageLabels?.[i] ?? null);
     }
   }
+
+  /**
+   * @param {PDFThumbnailView} thumbView
+   * @returns {Promise<PDFPageProxy | null>}
+   */
   async #ensurePdfPageLoaded(thumbView) {
     if (thumbView.pdfPage) {
       return thumbView.pdfPage;
@@ -180,9 +271,10 @@ class PDFThumbnailViewer {
       return pdfPage;
     } catch (reason) {
       console.error("Unable to get page for thumb view", reason);
-      return null;
+      return null; // Page error -- there is nothing that can be done.
     }
   }
+
   #getScrollAhead(visible) {
     if (visible.first?.id === 1) {
       return true;
@@ -191,10 +283,15 @@ class PDFThumbnailViewer {
     }
     return this.scroll.down;
   }
+
   forceRendering() {
     const visibleThumbs = this._getVisibleThumbs();
     const scrollAhead = this.#getScrollAhead(visibleThumbs);
-    const thumbView = this.renderingQueue.getHighestPriority(visibleThumbs, this._thumbnails, scrollAhead);
+    const thumbView = this.renderingQueue.getHighestPriority(
+      visibleThumbs,
+      this._thumbnails,
+      scrollAhead
+    );
     if (thumbView) {
       this.#ensurePdfPageLoaded(thumbView).then(() => {
         this.renderingQueue.renderView(thumbView);
@@ -204,3 +301,5 @@ class PDFThumbnailViewer {
     return false;
   }
 }
+
+export { PDFThumbnailViewer };
--- webpack/js/web/secondary_toolbar.js
+++ webpack/js/web/secondary_toolbar.js
@@ -1,268 +1,310 @@
+/* Copyright 2012 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-;// CONCATENATED MODULE: ./web/secondary_toolbar.js
+import {
+  CursorTool,
+  ScrollMode,
+  SpreadMode,
+  toggleCheckedBtn,
+  toggleExpandedBtn,
+} from "./ui_utils.js";
+import { PagesCountLimit } from "./pdf_viewer.js";
 
+/**
+ * @typedef {Object} SecondaryToolbarOptions
+ * @property {HTMLDivElement} toolbar - Container for the secondary toolbar.
+ * @property {HTMLButtonElement} toggleButton - Button to toggle the visibility
+ *   of the secondary toolbar.
+ * @property {HTMLButtonElement} presentationModeButton - Button for entering
+ *   presentation mode.
+ * @property {HTMLButtonElement} openFileButton - Button to open a file.
+ * @property {HTMLButtonElement} printButton - Button to print the document.
+ * @property {HTMLButtonElement} downloadButton - Button to download the
+ *   document.
+ * @property {HTMLAnchorElement} viewBookmarkButton - Button to obtain a
+ *   bookmark link to the current location in the document.
+ * @property {HTMLButtonElement} firstPageButton - Button to go to the first
+ *   page in the document.
+ * @property {HTMLButtonElement} lastPageButton - Button to go to the last page
+ *   in the document.
+ * @property {HTMLButtonElement} pageRotateCwButton - Button to rotate the pages
+ *   clockwise.
+ * @property {HTMLButtonElement} pageRotateCcwButton - Button to rotate the
+ *   pages counterclockwise.
+ * @property {HTMLButtonElement} cursorSelectToolButton - Button to enable the
+ *   select tool.
+ * @property {HTMLButtonElement} cursorHandToolButton - Button to enable the
+ *   hand tool.
+ * @property {HTMLButtonElement} documentPropertiesButton - Button for opening
+ *   the document properties dialog.
+ */
 
 class SecondaryToolbar {
-    constructor(options, eventBus) {
-      this.toolbar = options.toolbar;
-      this.toggleButton = options.toggleButton;
-      this.buttons = [{
+  #opts;
+
+  /**
+   * @param {SecondaryToolbarOptions} options
+   * @param {EventBus} eventBus
+   */
+  constructor(options, eventBus) {
+    this.#opts = options;
+    const buttons = [
+      {
         element: options.presentationModeButton,
         eventName: "presentationmode",
-        close: true
-      }, {
-        element: options.printButton,
-        eventName: "print",
-        close: true
+        close: true,
       },
+      { element: options.printButton, eventName: "print", close: true },
+      { element: options.downloadButton, eventName: "download", close: true },
+      { element: options.viewBookmarkButton, eventName: null, close: true },
+      { element: options.firstPageButton, eventName: "firstpage", close: true },
+      { element: options.lastPageButton, eventName: "lastpage", close: true },
       {
-        element: options.downloadButton,
-        eventName: "download",
-        close: true
-      }, {
-        element: options.viewBookmarkButton,
-        eventName: null,
-        close: true
-      }, {
-        element: options.firstPageButton,
-        eventName: "firstpage",
-        close: true
-      }, {
-        element: options.lastPageButton,
-        eventName: "lastpage",
-        close: true
-      }, {
         element: options.pageRotateCwButton,
         eventName: "rotatecw",
-        close: false
-      }, {
+        close: false,
+      },
+      {
         element: options.pageRotateCcwButton,
         eventName: "rotateccw",
-        close: false
-      }, {
+        close: false,
+      },
+      {
         element: options.cursorSelectToolButton,
         eventName: "switchcursortool",
-        eventDetails: {
-          tool: CursorTool.SELECT
-        },
-        close: true
-      }, {
+        eventDetails: { tool: CursorTool.SELECT },
+        close: true,
+      },
+      {
         element: options.cursorHandToolButton,
         eventName: "switchcursortool",
-        eventDetails: {
-          tool: CursorTool.HAND
-        },
-        close: true
-      }, {
+        eventDetails: { tool: CursorTool.HAND },
+        close: true,
+      },
+      {
         element: options.scrollPageButton,
         eventName: "switchscrollmode",
-        eventDetails: {
-          mode: ScrollMode.PAGE
-        },
-        close: true
-      }, {
+        eventDetails: { mode: ScrollMode.PAGE },
+        close: true,
+      },
+      {
         element: options.scrollVerticalButton,
         eventName: "switchscrollmode",
-        eventDetails: {
-          mode: ScrollMode.VERTICAL
-        },
-        close: true
-      }, {
+        eventDetails: { mode: ScrollMode.VERTICAL },
+        close: true,
+      },
+      {
         element: options.scrollHorizontalButton,
         eventName: "switchscrollmode",
-        eventDetails: {
-          mode: ScrollMode.HORIZONTAL
-        },
-        close: true
-      }, {
+        eventDetails: { mode: ScrollMode.HORIZONTAL },
+        close: true,
+      },
+      {
         element: options.scrollWrappedButton,
         eventName: "switchscrollmode",
-        eventDetails: {
-          mode: ScrollMode.WRAPPED
-        },
-        close: true
-      }, {
+        eventDetails: { mode: ScrollMode.WRAPPED },
+        close: true,
+      },
+      {
         element: options.spreadNoneButton,
         eventName: "switchspreadmode",
-        eventDetails: {
-          mode: SpreadMode.NONE
-        },
-        close: true
-      }, {
+        eventDetails: { mode: SpreadMode.NONE },
+        close: true,
+      },
+      {
         element: options.spreadOddButton,
         eventName: "switchspreadmode",
-        eventDetails: {
-          mode: SpreadMode.ODD
-        },
-        close: true
-      }, {
+        eventDetails: { mode: SpreadMode.ODD },
+        close: true,
+      },
+      {
         element: options.spreadEvenButton,
         eventName: "switchspreadmode",
-        eventDetails: {
-          mode: SpreadMode.EVEN
-        },
-        close: true
-      }, {
+        eventDetails: { mode: SpreadMode.EVEN },
+        close: true,
+      },
+      {
         element: options.documentPropertiesButton,
         eventName: "documentproperties",
-        close: true
-      }];
-      this.items = {
-        firstPage: options.firstPageButton,
-        lastPage: options.lastPageButton,
-        pageRotateCw: options.pageRotateCwButton,
-        pageRotateCcw: options.pageRotateCcwButton
-      };
-      this.eventBus = eventBus;
-      this.opened = false;
-      this.#bindClickListeners();
-      this.#bindCursorToolsListener(options);
-      this.#bindScrollModeListener(options);
-      this.#bindSpreadModeListener(options);
-      this.reset();
-    }
-    get isOpen() {
-      return this.opened;
-    }
-    setPageNumber(pageNumber) {
-      this.pageNumber = pageNumber;
-      this.#updateUIState();
-    }
-    setPagesCount(pagesCount) {
-      this.pagesCount = pagesCount;
-      this.#updateUIState();
-    }
-    reset() {
-      this.pageNumber = 0;
-      this.pagesCount = 0;
-      this.#updateUIState();
-      this.eventBus.dispatch("secondarytoolbarreset", {
-        source: this
+        close: true,
+      },
+    ];
+    if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
+      buttons.push({
+        element: options.openFileButton,
+        eventName: "openfile",
+        close: true,
       });
     }
-    #updateUIState() {
-      this.items.firstPage.disabled = this.pageNumber <= 1;
-      this.items.lastPage.disabled = this.pageNumber >= this.pagesCount;
-      this.items.pageRotateCw.disabled = this.pagesCount === 0;
-      this.items.pageRotateCcw.disabled = this.pagesCount === 0;
-    }
-    #bindClickListeners() {
-      this.toggleButton.addEventListener("click", this.toggle.bind(this));
-      for (const {
-        element,
-        eventName,
-        close,
-        eventDetails
-      } of this.buttons) {
-        element.addEventListener("click", evt => {
-          if (eventName !== null) {
-            this.eventBus.dispatch(eventName, {
-              source: this,
-              ...eventDetails
-            });
-          }
-          if (close) {
-            this.close();
-          }
-          this.eventBus.dispatch("reporttelemetry", {
-            source: this,
-            details: {
-              type: "buttons",
-              data: {
-                id: element.id
-              }
-            }
-          });
+
+    this.eventBus = eventBus;
+    this.opened = false;
+
+    // Bind the event listeners for click, cursor tool, and scroll/spread mode
+    // actions.
+    this.#bindListeners(buttons);
+
+    this.reset();
+  }
+
+  /**
+   * @type {boolean}
+   */
+  get isOpen() {
+    return this.opened;
+  }
+
+  setPageNumber(pageNumber) {
+    this.pageNumber = pageNumber;
+    this.#updateUIState();
+  }
+
+  setPagesCount(pagesCount) {
+    this.pagesCount = pagesCount;
+    this.#updateUIState();
+  }
+
+  reset() {
+    this.pageNumber = 0;
+    this.pagesCount = 0;
+    this.#updateUIState();
+
+    // Reset the Scroll/Spread buttons too, since they're document specific.
+    this.#scrollModeChanged({ mode: ScrollMode.VERTICAL });
+    this.#spreadModeChanged({ mode: SpreadMode.NONE });
+  }
+
+  #updateUIState() {
+    const {
+      firstPageButton,
+      lastPageButton,
+      pageRotateCwButton,
+      pageRotateCcwButton,
+    } = this.#opts;
+
+    firstPageButton.disabled = this.pageNumber <= 1;
+    lastPageButton.disabled = this.pageNumber >= this.pagesCount;
+    pageRotateCwButton.disabled = this.pagesCount === 0;
+    pageRotateCcwButton.disabled = this.pagesCount === 0;
+  }
+
+  #bindListeners(buttons) {
+    const { eventBus } = this;
+    const { toggleButton } = this.#opts;
+    // Button to toggle the visibility of the secondary toolbar.
+    toggleButton.addEventListener("click", this.toggle.bind(this));
+
+    // All items within the secondary toolbar.
+    for (const { element, eventName, close, eventDetails } of buttons) {
+      element.addEventListener("click", evt => {
+        if (eventName !== null) {
+          eventBus.dispatch(eventName, { source: this, ...eventDetails });
+        }
+        if (close) {
+          this.close();
+        }
+        eventBus.dispatch("reporttelemetry", {
+          source: this,
+          details: {
+            type: "buttons",
+            data: { id: element.id },
+          },
         });
-      }
-    }
-    #bindCursorToolsListener({
-      cursorSelectToolButton,
-      cursorHandToolButton
-    }) {
-      this.eventBus._on("cursortoolchanged", ({
-        tool
-      }) => {
-        toggleCheckedBtn(cursorSelectToolButton, tool === CursorTool.SELECT);
-        toggleCheckedBtn(cursorHandToolButton, tool === CursorTool.HAND);
       });
     }
-    #bindScrollModeListener({
+
+    eventBus._on("cursortoolchanged", this.#cursorToolChanged.bind(this));
+    eventBus._on("scrollmodechanged", this.#scrollModeChanged.bind(this));
+    eventBus._on("spreadmodechanged", this.#spreadModeChanged.bind(this));
+  }
+
+  #cursorToolChanged({ tool }) {
+    const { cursorSelectToolButton, cursorHandToolButton } = this.#opts;
+
+    toggleCheckedBtn(cursorSelectToolButton, tool === CursorTool.SELECT);
+    toggleCheckedBtn(cursorHandToolButton, tool === CursorTool.HAND);
+  }
+
+  #scrollModeChanged({ mode }) {
+    const {
       scrollPageButton,
       scrollVerticalButton,
       scrollHorizontalButton,
       scrollWrappedButton,
       spreadNoneButton,
       spreadOddButton,
-      spreadEvenButton
-    }) {
-      const scrollModeChanged = ({
-        mode
-      }) => {
-        toggleCheckedBtn(scrollPageButton, mode === ScrollMode.PAGE);
-        toggleCheckedBtn(scrollVerticalButton, mode === ScrollMode.VERTICAL);
-        toggleCheckedBtn(scrollHorizontalButton, mode === ScrollMode.HORIZONTAL);
-        toggleCheckedBtn(scrollWrappedButton, mode === ScrollMode.WRAPPED);
-        const forceScrollModePage = this.pagesCount > PagesCountLimit.FORCE_SCROLL_MODE_PAGE;
-        scrollPageButton.disabled = forceScrollModePage;
-        scrollVerticalButton.disabled = forceScrollModePage;
-        scrollHorizontalButton.disabled = forceScrollModePage;
-        scrollWrappedButton.disabled = forceScrollModePage;
-        const isHorizontal = mode === ScrollMode.HORIZONTAL;
-        spreadNoneButton.disabled = isHorizontal;
-        spreadOddButton.disabled = isHorizontal;
-        spreadEvenButton.disabled = isHorizontal;
-      };
-      this.eventBus._on("scrollmodechanged", scrollModeChanged);
-      this.eventBus._on("secondarytoolbarreset", evt => {
-        if (evt.source === this) {
-          scrollModeChanged({
-            mode: ScrollMode.VERTICAL
-          });
-        }
-      });
+      spreadEvenButton,
+    } = this.#opts;
+
+    toggleCheckedBtn(scrollPageButton, mode === ScrollMode.PAGE);
+    toggleCheckedBtn(scrollVerticalButton, mode === ScrollMode.VERTICAL);
+    toggleCheckedBtn(scrollHorizontalButton, mode === ScrollMode.HORIZONTAL);
+    toggleCheckedBtn(scrollWrappedButton, mode === ScrollMode.WRAPPED);
+
+    // Permanently *disable* the Scroll buttons when PAGE-scrolling is being
+    // enforced for *very* long/large documents; please see the `BaseViewer`.
+    const forceScrollModePage =
+      this.pagesCount > PagesCountLimit.FORCE_SCROLL_MODE_PAGE;
+    scrollPageButton.disabled = forceScrollModePage;
+    scrollVerticalButton.disabled = forceScrollModePage;
+    scrollHorizontalButton.disabled = forceScrollModePage;
+    scrollWrappedButton.disabled = forceScrollModePage;
+
+    // Temporarily *disable* the Spread buttons when horizontal scrolling is
+    // enabled, since the non-default Spread modes doesn't affect the layout.
+    const isHorizontal = mode === ScrollMode.HORIZONTAL;
+    spreadNoneButton.disabled = isHorizontal;
+    spreadOddButton.disabled = isHorizontal;
+    spreadEvenButton.disabled = isHorizontal;
+  }
+
+  #spreadModeChanged({ mode }) {
+    const { spreadNoneButton, spreadOddButton, spreadEvenButton } = this.#opts;
+
+    toggleCheckedBtn(spreadNoneButton, mode === SpreadMode.NONE);
+    toggleCheckedBtn(spreadOddButton, mode === SpreadMode.ODD);
+    toggleCheckedBtn(spreadEvenButton, mode === SpreadMode.EVEN);
+  }
+
+  open() {
+    if (this.opened) {
+      return;
     }
-    #bindSpreadModeListener({
-      spreadNoneButton,
-      spreadOddButton,
-      spreadEvenButton
-    }) {
-      const spreadModeChanged = ({
-        mode
-      }) => {
-        toggleCheckedBtn(spreadNoneButton, mode === SpreadMode.NONE);
-        toggleCheckedBtn(spreadOddButton, mode === SpreadMode.ODD);
-        toggleCheckedBtn(spreadEvenButton, mode === SpreadMode.EVEN);
-      };
-      this.eventBus._on("spreadmodechanged", spreadModeChanged);
-      this.eventBus._on("secondarytoolbarreset", evt => {
-        if (evt.source === this) {
-          spreadModeChanged({
-            mode: SpreadMode.NONE
-          });
-        }
-      });
+    this.opened = true;
+
+    const { toggleButton, toolbar } = this.#opts;
+    toggleExpandedBtn(toggleButton, true, toolbar);
+  }
+
+  close() {
+    if (!this.opened) {
+      return;
     }
-    open() {
-      if (this.opened) {
-        return;
-      }
-      this.opened = true;
-      toggleExpandedBtn(this.toggleButton, true, this.toolbar);
-    }
-    close() {
-      if (!this.opened) {
-        return;
-      }
-      this.opened = false;
-      toggleExpandedBtn(this.toggleButton, false, this.toolbar);
-    }
-    toggle() {
-      if (this.opened) {
-        this.close();
-      } else {
-        this.open();
-      }
+    this.opened = false;
+
+    const { toggleButton, toolbar } = this.#opts;
+    toggleExpandedBtn(toggleButton, false, toolbar);
+  }
+
+  toggle() {
+    if (this.opened) {
+      this.close();
+    } else {
+      this.open();
     }
   }
-  
+}
+
+export { SecondaryToolbar };
