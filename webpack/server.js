1da16960acd442fb7e09b1e74bd7933b7a478bc6
Erfan Abedi <erfanabedi.78@gmail.com>
2024-02-04 16:17:10 -0800

    improve node inital message

--- webpack/server.js
+++ webpack/server.js
@@ -53,5 +53,5 @@ app.get('/download/*', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/viewer.html`);
 });
